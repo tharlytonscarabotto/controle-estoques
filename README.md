22b49b1717d0c1989e8c8cef813ebb4104a683b1
tharlysc <tharlyton.scarabotto@outlook.com>
2023-05-02 15:50:54 -0300

    Enviar os inputs capturados para a lista de produtos

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Sistema básico onde são inseridos os produtos e quantidades para controle de e
 [X] Criar um formulário com campo para nome do produto, quantidade e um botão para submit
 [X] Montar uma lista com os produtos e suas respectivas quantidades (fixa primeiramente)
 [X] Capturar os inputs adicionados no formulário 
-[ ] Enviar os inputs capturados para a lista de produtos
-[ ] Mostrar os novos inputs visualmente na lista
+[X] Enviar os inputs capturados para a lista de produtos
+[X] Mostrar os novos inputs visualmente na lista
 [ ] Gravar os produtos adicionados na localStorage
 [ ] Fazer com que o que está na localStorage seja jogado para a lista automaticamente
 [ ] Possibilitar a atualização destes estoques
--- script/main.js
+++ script/main.js
@@ -1,15 +1,24 @@
 const formulario = document.querySelector('#formulario')
 
-const produto = document.querySelector('#produto');
-const quantidade = document.querySelector('#quantidade')
-
 formulario.addEventListener('submit', (evento)=>{
     evento.preventDefault()
 
-    criaProduto()
+    criaProduto(evento.target.elements['produto'].value, evento.target.elements['quantidade'].value)
 })
 
-function criaProduto() {
-    console.log(produto.value)
-    console.log(quantidade.value)
+function criaProduto(produto, quantidade){
+    console.log(produto)
+    console.log(quantidade)
+
+    // <li class="item"><strong>3</strong>Azeitona</li>
+    const novoProduto = document.createElement('li')
+    novoProduto.classList.add('item')
+
+    const quantidadeProduto = document.createElement('strong')
+    quantidadeProduto.innerHTML = quantidade
+
+    novoProduto.appendChild(quantidadeProduto)
+    novoProduto.innerHTML += produto
+
+    document.getElementById('lista').appendChild(novoProduto)
 }
